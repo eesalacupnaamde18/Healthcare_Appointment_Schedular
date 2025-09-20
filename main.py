21c6764f9e29ad390fca0f0c6a26663614cf201b
eesalacupnaamde18 <shiekhroshni5@gmail.com>
2025-09-20 14:50:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware  # <-- import this
 from .database import Base, engine
 from .routes import users, doctors, appointments
 
 app = FastAPI(title="Medical Appointment Booking")
+
+# Allow frontend to access backend
+origins = [
+    "http://127.0.0.1:5500",  # If you run index.html with Live Server in VS Code
+    "http://localhost:5500",
+    "*",  # Optional: allow all origins (for testing only)
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Create tables
 Base.metadata.create_all(bind=engine)
@@ -14,4 +30,5 @@ app.include_router(appointments.router)
 
 @app.get("/")
 def root():
-    return {"message": "Welcome to the Medical Appointment Booking API!"}
+    return {"message": "Welcome to Medical Appointment Booking API!"}
+
